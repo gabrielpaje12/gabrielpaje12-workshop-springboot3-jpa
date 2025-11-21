285cec37b7d36c2ccff2ef3ed86d9016abb689e2
gabrielpaje12 <gabrieloliveira362978@gmail.com>
2025-11-20 22:31:15 -0300

    H2 database, test profile, JPA

--- src/main/java/com/APIweb/course/entities/User.java
+++ src/main/java/com/APIweb/course/entities/User.java
@@ -1,14 +1,19 @@
 package com.APIweb.course.entities;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "tb_user")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
