ea33a178263f207676c50aa2c32b1f44b87854b5
gabrielpaje12 <gabrieloliveira362978@gmail.com>
2025-11-20 23:01:28 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/APIweb/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.APIweb.course.repositories;
+
+import com.APIweb.course.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
