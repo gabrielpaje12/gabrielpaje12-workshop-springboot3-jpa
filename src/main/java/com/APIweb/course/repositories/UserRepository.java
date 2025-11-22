5f03d1e1ac439e68b516e0b1545a61629bbfaf46
gabrielpaje12 <gabrieloliveira362978@gmail.com>
2025-11-21 22:28:09 -0300

    Service layer, component registration

--- src/main/java/com/APIweb/course/repositories/UserRepository.java
+++ src/main/java/com/APIweb/course/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.APIweb.course.repositories;
 
 import com.APIweb.course.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
